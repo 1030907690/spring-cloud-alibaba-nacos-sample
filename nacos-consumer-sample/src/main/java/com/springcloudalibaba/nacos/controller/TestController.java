cace61ed3a5de2363ea09911f64b67290da81810
1030907690 <1030907690@qq.com>
2020-07-08 17:01:22 +0800

    增加注释

--- nacos-consumer-sample/src/main/java/com/springcloudalibaba/nacos/controller/TestController.java
+++ nacos-consumer-sample/src/main/java/com/springcloudalibaba/nacos/controller/TestController.java
@@ -3,11 +3,11 @@ package com.springcloudalibaba.nacos.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // @RestController注解是@Controller+@ResponseBody
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/test")  // 标记是该方法的请求
     public String test() {
-        return null;
+        return null; // 暂且先返回null，下一步我们接入基本的远程调用，去调用提供者
     }
 }
--- nacos-provider-sample8081/src/main/java/com/springcloudalibaba/nacos/controller/TestController.java
+++ nacos-provider-sample8081/src/main/java/com/springcloudalibaba/nacos/controller/TestController.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // @RestController注解是@Controller+@ResponseBody
 public class TestController {
 
-    @Value("${server.port}")
-    private String port;
 
-    @RequestMapping("/test")
+    @Value("${server.port}")
+    private String port; // 获取配置文件中写的程序端口号
+
+
+    @RequestMapping("/test") // 标记是该方法的请求
     public String test() {
-        return "hello world test " + port;
+        return "hello world test " + port;  // 返回值是一个字符串，因为用了@RestController所以不必额外加@ResponseBody了
     }
 }
